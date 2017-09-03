88cd863d1bf5eab8a5bb33532770c091dd9b0e38
1000ch <shogo.sensui@gmail.com>
2017-09-03 23:18:43 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+import FancyButton from './fancy-button.js';
+import FancyDivButton from './fancy-div-button.js';
+
+customElements.define('fancy-button', FancyButton);
+customElements.define('fancy-div-button', FancyDivButton);
